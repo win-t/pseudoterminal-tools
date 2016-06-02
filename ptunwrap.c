b06da8077fb5a1a18139a67f30cf272d66d12f4e
Kurniagusta Dwinto <kurniagusta.dwinto@traveloka.com>
2016-06-02 16:12:54 +0700

    add signal handling, only allow if input is tty

--- ptunwrap.c
+++ ptunwrap.c
@@ -14,31 +14,78 @@
 
 #define LOCAL_BUF_SIZE 1024
 
+static volatile int closing = 0;
+static void cleanup_handler(int sig);
+static int start_upstream(int up, int down);
+static int start_downstream(int up, int down);
+
 int main(int argc, char *argv[]) {
     if(argc < 3) fail_err(EINVAL);
 
     int up = ensure(open(argv[1], O_WRONLY));
     int down = ensure(open(argv[2], O_RDONLY));
 
-    int ttyfd = -1;
     struct termios oldterm, newterm;
+    struct sigaction cleanup;
 
-    for(int i = 0; i < 3; ++i) {
-        if(isatty(i)) {
-            ttyfd = i;
-            break;
+    memset(&cleanup, 0, sizeof(cleanup));
+    cleanup.sa_handler = cleanup_handler;
+
+    ensure(sigaction(SIGTERM, &cleanup, NULL));
+    ensure(sigaction(SIGINT, &cleanup, NULL));
+
+    if(!isatty(0)) fail_err(EINVAL);
+
+    ensure(tcgetattr(0, &oldterm));
+    newterm = oldterm;
+    cfmakeraw(&newterm);
+    ensure(tcsetattr(0, TCSADRAIN, &newterm));
+
+    int uppid = start_upstream(up, down);
+    int downpid = start_downstream(up, down);
+
+    while(1) {
+        int tmp;
+        int rc = waitpid(-1, &tmp, 0);
+        IF_err(rc) {
+            if(errno == ECHILD) break;
+            else if (errno == EINTR && closing) {
+                kill(uppid, SIGKILL);
+                kill(downpid, SIGKILL);
+            } else {
+                kill(uppid, SIGKILL);
+                waitpid(-1, NULL, WNOHANG);
+                kill(downpid, SIGKILL);
+                waitpid(-1, NULL, WNOHANG);
+                fail_eno();
+            }
+        } else {
+            if (WEXITSTATUS(tmp) != 0) {
+                if (!closing) {
+                    if(rc == uppid) uppid = start_upstream(up, down);
+                    else if(rc == downpid) downpid = start_downstream(up, down);
+                }
+            } else {
+                closing = 1;
+                if(rc == uppid) kill(downpid, SIGKILL);
+                else if(rc == downpid) kill(uppid, SIGKILL);
+            }
         }
     }
 
-    if(ttyfd != -1) {
-        ensure(tcgetattr(ttyfd, &oldterm));
-        newterm = oldterm;
-        cfmakeraw(&newterm);
-        ensure(tcsetattr(ttyfd, TCSANOW, &newterm));
-    }
+    ensure(tcsetattr(0, TCSADRAIN, &oldterm));
 
-    int uppid = ensure(fork());
-    if(uppid == 0) {
+    return 0;
+}
+
+static void cleanup_handler(int sig) {
+    closing = 1;
+}
+
+static int start_upstream(int up, int down) {
+    int ret = ensure(fork());
+    if(ret == 0) {
+        ensure(close(down));;
         char buf[LOCAL_BUF_SIZE];
         while(1) {
             char *bufptr = buf;
@@ -51,9 +98,13 @@ int main(int argc, char *argv[]) {
         }
         _exit(0);
     }
+    return ret;
+}
 
-    int downpid = ensure(fork());
-    if(downpid == 0) {
+static int start_downstream(int up, int down) {
+    int ret = ensure(fork());
+    if(ret == 0) {
+        ensure(close(up));
         char buf[LOCAL_BUF_SIZE];
         while(1) {
             char *bufptr = buf;
@@ -66,19 +117,5 @@ int main(int argc, char *argv[]) {
         }
         _exit(0);
     }
-
-    while(1) {
-        int rc = wait(NULL);
-        IF_err(rc) {
-            if(errno == ECHILD) break;
-            else fail_eno();
-        }
-        if(rc == downpid) {
-            kill(uppid, SIGKILL);
-        }
-    }
-
-    if(ttyfd != -1) ensure(tcsetattr(ttyfd, TCSANOW, &oldterm));
-
-    return 0;
+    return ret;
 }
